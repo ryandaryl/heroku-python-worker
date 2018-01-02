d7e05976fcdfa798d0402f2d04e9d6e21a6a6cd2
rydamil60000g <ryandarylmills@gmail.com>
2018-01-02 09:25:37 +0000

    Added code from python-rq article at Heroku devcenter.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
+from rq import Queue
+from worker import conn
+from utils import count_words_at_url
+
 app = Flask(__name__)
+q = Queue(connection=conn)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    result = q.enqueue(count_words_at_url, 'http://heroku.com')
+    return result
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

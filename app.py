69eadf1660f8456116bb3b2b78bebf2eb816f2cb
rydamil60000g <ryandarylmills@gmail.com>
2018-01-03 05:35:15 +0000

    Return status of job as JSON.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from rq import Queue
 from worker import conn
 from utils import count_words_at_url
@@ -7,14 +7,26 @@ from utils import count_words_at_url
 app = Flask(__name__)
 q = Queue(connection=conn)
 
+def get_status(job):
+    return {
+        'id': job.id,
+        'result': job.result,
+        'status': 'failed' if job.is_failed else 'pending' if job.result == None else 'completed'
+    }
+
 @app.route("/")
 def hello():
-    found_job = request.args.get('job')
-    if found_job:
-        return str(q.fetch_job(found_job).result)
+    query_id = request.args.get('job')
+    if query_id:
+        found_job = q.fetch_job(query_id)
+        if found_job:
+            output = get_status(found_job)
+        else:
+            output = { 'id': None, 'error_message': 'No job exists with the id number ' + query_id }
     else:
         new_job = q.enqueue(count_words_at_url, 'http://heroku.com')
-        return new_job.id
+        output = get_status(new_job)
+    return jsonify(output)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
